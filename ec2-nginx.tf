1cfb248403bcbd5bf6b7687eaf5260e0d8ef251c
Hammad <hammadmughal0@gmail.com>
2018-10-15 11:02:06 +0500

    Update ec2-nginx.tf

--- ec2-nginx.tf
+++ ec2-nginx.tf
@@ -1,4 +1,4 @@
-# Template to spin up ec2 instance with nginx installed.
+# Template to spin up ec2 instance (Amazon linux) with nginx installed.
 
 variable "private_key_path"{}
 
@@ -17,9 +17,21 @@ resource "aws_instance" "nginx"{
 	ami = "ami-c58c1dd3"
 	instance_type = "t2.micro"
 
-}
+
 
 connection {
 	user = "ec2-user"
-	private_key = $(file(var.priavate_key_path))
+	private_key = ${file(var.priavate_key_path)}
+}
+
+provisioner "remote-exec"{
+	
+	inline = {
+		"sudo yum install nginx -y",
+		"sudo service nginx start"
+	}
+}
+}
+output "aws_instance_public_dns"{
+	value = "${aws_instance.nginx.public_dns}"
 }
